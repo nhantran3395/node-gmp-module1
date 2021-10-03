7c665f4abad9ea776962c456a154397a768b1dd5
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-10-03 16:24:03 +0700

    feat: add task 1

--- task1/index.ts
+++ task1/index.ts
@@ -1,1 +1,24 @@
-console.log("task 1");
+const readline = require("readline");
+
+const lineReader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+lineReader.on("line", (line: string) => {
+  const lineReversed = reverseLine(line);
+  console.log(lineReversed);
+});
+
+const reverseLine = (line: string): string => {
+  return line
+    .split(" ")
+    .map((word) => reverseWord(word))
+    .reverse()
+    .join(" ");
+};
+
+const reverseWord = (word: string): string => {
+  return word.split("").reverse().join("");
+};
