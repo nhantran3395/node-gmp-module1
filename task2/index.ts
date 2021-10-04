49ed9e86acd1622303d551d11ee1af388cd427f8
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-10-04 11:09:57 +0700

    feat: add exercise 2

--- task2/index.ts
+++ task2/index.ts
@@ -1,1 +1,24 @@
-console.log("task 2");
+import csvReader from "csvtojson";
+import fs from "fs";
+
+const INPUT_CSV_FILE_PATH = "./csv/task2.csv";
+const OUTPUT_TXT_FILE_PATH = "./csv/task2.txt";
+
+const writeCsvToTxt = async () => {
+  let data: {}[] = [];
+
+  try {
+    data = await csvReader().fromFile(INPUT_CSV_FILE_PATH);
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    fs.writeFileSync(OUTPUT_TXT_FILE_PATH, JSON.stringify(data));
+    console.log(`successfully written to ${OUTPUT_TXT_FILE_PATH}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+writeCsvToTxt();
