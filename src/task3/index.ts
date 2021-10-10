eea59c1b0dbc5404108b795e369056953844ba96
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-10-10 13:47:56 +0700

    feat: add task3

--- src/task3/index.ts
+++ src/task3/index.ts
@@ -1,1 +1,20 @@
-console.log("task 3");
+import console from "console";
+import csvReader from "csvtojson";
+import { createReadStream, createWriteStream } from "fs";
+
+import BookDataConverter from "./BookDataConverter";
+import { BookInputDTO, BookOutputDTO } from "../shared/model";
+
+const INPUT_CSV_FILE_PATH = "./csv/bookData.csv";
+const OUTPUT_TXT_FILE_PATH = "./csv/task3bookOutput.txt";
+
+const readStream = createReadStream(INPUT_CSV_FILE_PATH);
+const writeStream = createWriteStream(OUTPUT_TXT_FILE_PATH);
+const bookDataConverter = new BookDataConverter();
+
+readStream
+  .pipe(csvReader())
+  .pipe(writeStream)
+  .on(`error`, (error: any) => {
+    console.error(error);
+  });
